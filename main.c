3989fc05bf5ade3615ce12bb1af9a87025162d66
Mickael Marcotte <mmarcott@c1r8p12.42quebec.com>
2023-05-24 15:08:04 -0400

    fuck

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmarcott <mmarcott@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 02:49:57 by mmarcott          #+#    #+#             */
-/*   Updated: 2023/05/24 02:54:06 by mmarcott         ###   ########.fr       */
+/*   Updated: 2023/05/24 14:53:41 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int	main(void)
 	str[0] = 'd';
 	str[1] = 0;
 	printf("%s\n", str);
-	mms_kill("Qutting the program !\n", true, EXIT_SUCCESS);
+	// mms_kill("free all the peoples\n", true, EXIT_SUCCESS);
+	str = mms_free(str);
 }
