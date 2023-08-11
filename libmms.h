00b0728ce4e9a56b3ec2bef935c4f47f5fe59377
Mickael Marcotte <mmarcott@c2r5p10.42quebec.com>
2023-08-11 11:39:10 -0400

    Changed the libmms.h for better function documentation

--- libmms.h
+++ libmms.h
@@ -6,7 +6,7 @@
 /*   By: mmarcott <mmarcott@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 01:48:36 by mmarcott          #+#    #+#             */
-/*   Updated: 2023/05/27 19:03:25 by mmarcott         ###   ########.fr       */
+/*   Updated: 2023/08/11 11:37:25 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,33 @@ typedef struct s_pointer
 	void				*(*f)(size_t, size_t);
 }				t_pointer;
 
-// Delete all the used memory from the manager.
+/// @brief This function deletes all the pointers assigned with the lib.
+/// @param message The message to print when quitting.
+/// @param quit Quitting of not.
+/// @param code The exit code when quitting.
 void		mms_kill(char *message, bool quit, int code);
 
+/// @brief Allocate memory.
+/// @param size the size in bytes.
+/// @param typesize The type size in bytes.
+/// @return An allocated pointer.
 void		*mms_alloc(size_t size, size_t typesize);
 
+/// @brief Prints the pointers list.
 void		print_list(void);
 
+/// @brief Frees a single pointer allocated with the lib.
+/// @param ptr The pointer to free.
+/// @return NULL in all cases.
 void		*mms_free(void *ptr);
 
+/// @brief Do not use for your project, this function is used 
+//				to get the structure of the lib.
+/// @return The lib's structure.
 t_pointer	*get_data_mms(void);
 
+/// @brief Used to change the default function for allocation.
+/// @param f The function ex: calloc. Default is calloc.
 void		mms_set_alloc_fn(void *(*f)(size_t, size_t));
 
 #endif
